ae020755c187db8934572e003f29f63a6aa509dd
Phil <pnicolet@uwo.ca>
2019-06-17 21:57:18 -0400

    pre-heroku

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
