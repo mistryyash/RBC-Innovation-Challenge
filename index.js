10b3f1f687f141b599919349878e3175171f5d52
Phil <pnicolet@uwo.ca>
2019-06-17 20:56:55 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(express.json());
+
+app.post("/slack", (req,res) => {
+    console.log(req.body);
+    res.status(200).send({"challenge":req.body.challenge});
+});
+
+app.listen(port, () => console.log("we up"));
